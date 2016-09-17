b67d178ad39fb374032578543967fe8888d3af2c
Sebastian Green-Husted <ricecake@tfm.nu>
2016-09-16 22:50:41 -0400

    add static content support

--- apps/maestro_web/src/maestro_web_app.erl
+++ apps/maestro_web/src/maestro_web_app.erl
@@ -15,11 +15,24 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    maestro_web_sup:start_link().
+	case maestro_web_sup:start_link() of
+		{ok, Pid} ->
+			Dispatch = cowboy_router:compile([
+				{'_', [
+					{"/ws/",          maestro_web_msg_handler, #{}},
+					{"/",             maestro_web_page, index},
+					{"/static/[...]", cowboy_static, {priv_dir, maestro_web, "static/"}}
+				]}
+			]),
+			{ok, _} = cowboy:start_clear(maestro_web, 25, [{ip, {127,0,0,1}}, {port, 8001}], #{
+				env => #{ dispatch => Dispatch }
+			}),
+			{ok, Pid}
+	end.
 
 %%--------------------------------------------------------------------
 stop(_State) ->
-    ok.
+	ok.
 
 %%====================================================================
 %% Internal functions
