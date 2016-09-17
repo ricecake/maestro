4502af770bd9990b752bf23b6f5c9c9802558e5a
Sebastian Green-Husted <ricecake@tfm.nu>
2016-09-17 19:41:26 -0400

    get the basic bits ready

---
+++ apps/maestro_web/priv/static/js/common.js
@@ -0,0 +1,11 @@
+requirejs.config({
+	baseUrl: 'static/js',
+	paths: {
+		jquery: 'lib/ext/jquery-2.1.1.min',
+		underscore: 'lib/ext/underscore-min',
+		KnotConn: 'lib/ext/connection',
+		text: 'lib/ext/text',
+		tpl: 'lib/ext/tpl',
+		template: '../template'
+	}
+});
